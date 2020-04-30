89a7a0ec57f24820d463857d900fa11c0cc797ca
kimminseo3470 <ms990905@naver.com>
2020-05-01 01:32:42 +0900

    예외 설정하여 error message 출력

--- timediff.c
+++ timediff.c
@@ -9,5 +9,36 @@ typedef struct time {
 
 int main(void) {
 	printf("timediff");
+
+	int d, h, m;
+	int total1, total2;
+	int result;
+	time time1, time2;
+
+	while (1) {
+		printf("Enter date1[1~30], hour1[0~23], minute1[0~59]: ");
+		scanf("%d %d %d", &d, &h, &m);
+		if (d < 1 || d>30 || h < 0 || h>23 || m < 0 || m>59)
+			printf("Time error\n");
+		else {
+			time1.date = d;
+			time1.hour = h;
+			time1.minute = m;
+			break;
+		}
+	}
+
+	while (1) {
+		printf("Enter date2[1~30], hour2[0~23], minute2[0~59]: ");
+		scanf("%d %d %d", &d, &h, &m);
+		if (d < 1 || d>30 || h < 0 || h>23 || m < 0 || m>59)
+			printf("Time error\n");
+		else {
+			time2.date = d;
+			time2.hour = h;
+			time2.minute = m;
+			break;
+		}
+	}
 	return 0;
 }
