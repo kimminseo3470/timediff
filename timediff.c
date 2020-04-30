55695883e8a9a2d7025ed604d4f14f924a751ff6
kimminseo3470 <ms990905@naver.com>
2020-05-01 01:30:43 +0900

    scanf로 변수 할당

--- timediff.c
+++ timediff.c
@@ -9,5 +9,23 @@ typedef struct time {
 
 int main(void) {
 	printf("timediff");
+
+	int d, h, m;
+	int total1, total2;
+	int result;
+	time time1, time2;
+
+	printf("Enter date1[1~30], hour1[0~23], minute1[0~59]: ");
+	scanf("%d %d %d", &d, &h, &m);
+	time1.date = d;
+	time1.hour = h;
+	time1.minute = m;
+
+	printf("Enter date2[1~30], hour2[0~23], minute2[0~59]: ");
+	scanf("%d %d %d", &d, &h, &m);
+
+	time2.date = d;
+	time2.hour = h;
+	time2.minute = m;
 	return 0;
 }
